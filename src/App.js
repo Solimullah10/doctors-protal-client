a6127a193d04f4af141c6b3f7ca4b2672ffdb170
Solimullah10 <mdsolimullah10@gmail.com>
2022-07-27 22:38:40 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Appointment from './pages/Appointment/Appointment'
+import Reviews from './pages/Reviews/Reviews'
+import ContactUs from './pages/ContactUs/ContactUs'
+import Login from './pages/Login/Login'
 
 function App() {
   return (
@@ -10,7 +14,13 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/contact' element={<ContactUs></ContactUs>}></Route>
+
+
       </Routes>
     </div>
   );
